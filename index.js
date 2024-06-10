c1bd61affcb9b974e13bf49c2cd5b7a33c58cece
RohanKk007 <rohankarki057@gmail.com>
2024-06-10 19:09:45 +0545

    improved artist page

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-let cards = document.querySelector(".card");
- cards.addEventListener("click", ()=> {
-    window. location = "index1.html";
-})
+//visit index1(Artist page)  on  card click
+let card = document.getElementsByClassName("card");
+for (i=0; i<card.length; i++) {
+            card[i].addEventListener("click", ()=> {
+            window. location = "index1.html";
+        })
+}
+   
+
+
+
---
+++ index1.js
@@ -0,0 +1,18 @@
+//return to index0(Home page) on home button click
+let arrow_btn = document.querySelector(".arrow_btn1");
+let homeButton = document.querySelector("#HomeButton");
+homeButton.addEventListener("click", redirect);
+arrow_btn.addEventListener("click", redirect);
+
+function redirect() {
+        window. location = "index0.html";
+}
+
+
+//banner appear on song click
+let element = document.getElementsByClassName('song');
+for(var i=0; i<element.length; i++){
+        element[i].addEventListener('click', () => {
+                document.querySelector(".songBanner").style.display = "flex";
+        })
+}
